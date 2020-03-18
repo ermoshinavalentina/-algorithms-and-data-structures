f0a78070f4f3c2b00260ec0bea354cf21b7a0fdb
ermoshinavalentina <61473932+ermoshinavalentina@users.noreply.github.com>
2020-03-19 00:50:27 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -16,3 +16,27 @@ int main() {
 	else
 		printf_s("Не попадает");
 }
+
+//график
+
+#include <iostream>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    float R;
+    float x;
+    float y;
+    std::cout << "Введите R = " << std::endl;
+    std::cin >> R;
+    std::cout << "Введите x = " << std::endl;
+    std::cin >> x;
+    std::cout << "Введите y = " << std::endl;
+    std::cin >> y;
+    if ((x >= -10 and x < 0) || (x >= 0 and x < 3) || (x >= 3 and x < 6))
+       printf_s("Точка попадает в область");
+    else  printf_s("Точка не попадает в область");
+}
+
+
